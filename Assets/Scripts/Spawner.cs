313e576c003f645409df94b1b62c107793f1ab61
2dMark <mukhinmr92@gmail.com>
2023-12-05 21:57:45 +0300

    script editing

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,36 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _template;
+    [SerializeField] private Enemy _template;
     [SerializeField] private float _spawnTime;
     [SerializeField] private int _spawnsNumber;
     [SerializeField] private float _spawnSpacing;
 
     private GameObject[] _spawns;
-    private float _timer;
 
     private void Awake()
     {
-        _timer = _spawnTime;
         _spawns = new GameObject[_spawnsNumber];
 
         for (int i = 0; i < _spawns.Length; i++)
         {
             _spawns[i] = new GameObject($"Spawn {i + 1}");
-            _spawns[i].transform.Translate(i * _spawnSpacing, 0, 0);
+            _spawns[i].transform.SetLocalPositionAndRotation(transform.localPosition, transform.localRotation);
+            _spawns[i].transform.transform.Translate(i * _spawnSpacing, 0, 0);
         }
     }
 
-    private void Update()
+    private void Start()
     {
-        _timer -= Time.deltaTime;
+        StartCoroutine(Spawn(_spawnTime));
+    }
 
-        if (_timer <= 0)
+    private IEnumerator Spawn(float cooldown)
+    {
+        WaitForSeconds timer = new(cooldown);
+
+        while (true)
         {
-            _timer = _spawnTime;
-
             Instantiate(_template, _spawns[Random.Range(0, _spawns.Length)].transform);
+
+            yield return timer;
         }
     }
 }
